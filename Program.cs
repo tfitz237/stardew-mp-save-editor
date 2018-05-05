3a3e230b385fbe5cf62f1a617399c29a14f88cc4
Tom Fitzpatrick <tfitz237@gmail.com>
2018-05-05 17:47:51 -0400

    find blankCabin

--- Program.cs
+++ Program.cs
@@ -8,18 +8,17 @@ namespace stardew
     {
         static void Main(string[] args)
         {
-            var path = args[0].Replace("\\", "/").TrimEnd();
-            Console.WriteLine(path);
+            var path = args.Count() > 0 ? args[0].Replace("\\", "/").TrimEnd() : "./samples/TemplateNa_185230783";
             XNamespace ns = "http://www.w3.org/2001/XMLSchema-instance";
-            var d = XDocument.Load(path).Descendants();
+            var d = XDocument.Load(path).Element("SaveGame").Elements();
             var player = d.First(x => x.Name == "player");
-            var locations = d.First(x => x.Name == "locations").Descendants();
+            var locations = d.First(x => x.Name == "locations").Elements();
             var farm = locations.Single(x => x.Attribute(ns + "type")?.Value == "Farm");
-            var buildings = farm.Element("buildings").Descendants();
+            var buildings = farm.Element("buildings").Elements();
             var cabins = buildings.Where(x => x.Element("indoors").Attribute(ns + "type")?.Value == "Cabin");
-            var blankCabin = cabins.Single(x => x.Element("indoors")
+            var blankCabin = cabins.First(x => x.Element("indoors")
                                             .Element("farmhand")
-                                            .Element("name").Value == null
+                                            .Element("name").IsEmpty
                                         );
             var blankFarmHand = blankCabin.Element("indoors").Element("farmhand");
             
