d32da920f8517cbdb3a4ceeeaacb7cee5770f7d5
Eric Schwarz <egs3690@gmail.com>
2018-05-06 16:36:21 -0500

    If no save file argument is provided, program asks user to select a save file from their AppData folder. SaveGame.CreateNewCabin uses blank Cabin from samplestemplate to avoid assumption that the user provided save file will have a blank cabin.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace stardew
 {
     class Program
     {
+        static String GetSaveFile(String path) {
+            String userSelection = "";
+            Dictionary<String, String> saveFiles = new Dictionary<String, String>();
+            
+            Console.WriteLine("Select a save file: ");
+            while(!saveFiles.ContainsKey(userSelection)) {
+                saveFiles = new Dictionary<String, String>();
+                int fileCount = 0;
+                foreach(String saveFolder in Directory.GetDirectories(path)) {
+                    fileCount++;
+                    String saveFileName = Regex.Matches(saveFolder, @"[^\\]*$").First().ToString();
+                    String saveFilePath = String.Format("{0}\\{1}", saveFolder, saveFileName);
+                    saveFiles.Add(fileCount.ToString(), saveFilePath);
+                    Console.WriteLine(String.Format("{0}. {1}", fileCount, saveFileName));
+                }
+                userSelection = Console.ReadLine();
+            }
+
+            return saveFiles[userSelection];
+        }
+
         static void Main(string[] args)
         {   
             var newPlayers = args.Count() > 0 && Int32.TryParse(args[0], out var ignore) ? Int32.Parse(args[0]) : 1;
-            var path = args.Count() > 1 ? args[1].Replace("\\", "/").TrimEnd() : "./samples/TemplateNa_185230783";
+            var path = args.Count() > 1 ? args[1].Replace("\\", "/").TrimEnd() : GetSaveFile(String.Format("C:\\Users\\{0}\\AppData\\Roaming\\StardewValley\\Saves", Environment.UserName));
+            Console.WriteLine(path);
             var game = new SaveGame(path);
-            
+
             while (newPlayers > 0) {
                 var cabins = game.Cabins;
                 var cabinClone = game.CreateNewCabin();
--- saves/SaveGame.cs
+++ saves/SaveGame.cs
@@ -8,7 +8,8 @@ namespace stardew {
         XNamespace ns = "http://www.w3.org/2001/XMLSchema-instance";
         private XDocument _doc {get;set;}
         private IEnumerable<XElement> _saveGame {get; set;}
-        private Random rand = new Random();
+        private Random random = new Random();
+        private String templatePath = "samples\\template";
         public SaveGame (string path) {
             try {
                 _doc = XDocument.Load(path);
@@ -21,6 +22,10 @@ namespace stardew {
             }
         }
         
+        public XElement Host { get => _saveGame
+            .First(x => x.Name == "player");
+        }
+
         public XElement Farm { get => _saveGame
             .First(x => x.Name == "locations")
             .Elements()
@@ -37,17 +42,16 @@ namespace stardew {
             .Where(x => 
                 x.Element("indoors")
                 .Attribute(ns + "type")?.Value == "Cabin"); 
-        }               
+        }
         
         public void AddCabin(XElement cabin) {
             Farm.Element("buildings").Add(cabin);
         }
 
         public XElement CreateNewCabin() {
-            var blankCabin = Cabins.First(x => x.Element("indoors")
-                                        .Element("farmhand")
-                                        .Element("name").IsEmpty
-                                    ); 
+            XDocument template = XDocument.Load(templatePath);
+            var blankCabin = template.Element("template")
+                .Element("Building");
             return new XElement(blankCabin);
         }
 
@@ -55,25 +59,35 @@ namespace stardew {
             var uniqueName = String.Format("Cabin{0}", Guid.NewGuid().ToString());
             Console.WriteLine(uniqueName);
             cabin.Element("indoors").Element("uniqueName").Value = uniqueName;
-            cabin.Element("indoors")
-                .Element("farmhand")
+            getFarmhand(cabin)
                 .Element("homeLocation").Value = uniqueName;
             return cabin;
         }
 
         public XElement ReplaceMultiplayerId(XElement cabin) {
             byte[] buffer = new byte[8];
-            rand.NextBytes(buffer);
+            random.NextBytes(buffer);
             var uniqueMultiplayerId = BitConverter.ToInt64(buffer, 0);
-            cabin.Element("indoors")
-                .Element("farmhand")
+            getFarmhand(cabin)
                 .Element("UniqueMultiplayerID")
                 .Value= uniqueMultiplayerId.ToString();
             return cabin;
         }
 
+        private XElement UpdateFarmhand(XElement cabin) {
+            var farmhand = getFarmhand(cabin);
+            farmhand.Element("farmName").Value = Host.Element("farmName").Value;
+            farmhand.Element("money").Value = Host.Element("money").Value;
+            return cabin;
+        }
+            
         public void SaveFile() {
             _doc.Save("./saves/JustSaved.xml");
+        }
+
+        private XElement getFarmhand(XElement cabin) {
+            return cabin.Element("indoors")
+                .Element("farmhand");
         }
     }
 }
