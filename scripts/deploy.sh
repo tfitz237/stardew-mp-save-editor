ae51ffcde9f84b0a4f91b565e2aef879d65a0b6b
Tom Fitzpatrick <tfitz237@gmail.com>
2018-05-15 18:31:13 +0000

    Fix zip output path for travisci integration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 cd Stardew.MPSaveEditor
 dotnet publish -c Release -r win10-x64 -o $TRAVIS_BUILD_DIR/releases/Stardew.MPSaveEditor
-zip -r $TRAVIS_BUILD_DIR/releases/Stardew.MPSaveEditor-$TRAVIS_BUILD_NUMBER.zip ${TRAVIS_BUILD_DIR}/releases/Stardew.MPSaveEditor
+zip -r $TRAVIS_BUILD_DIR/releases/Stardew.MPSaveEditor-$TRAVIS_BUILD_ID.zip ${TRAVIS_BUILD_DIR}/releases/Stardew.MPSaveEditor
