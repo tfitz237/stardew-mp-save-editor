1b6e4dc477eab9ca596f1c960fb3cb3cc996dbab
Tom Fitzpatrick <tfitz237@gmail.com>
2018-05-15 18:50:27 +0000

    Only deploy if it can find a tag to give it

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -1,11 +1,14 @@
-tag=$(git tag | tail -1)
-echo "old tag: $tag"
 reg='^([0-9])+\.([0-9])+\.([0-9]+)$'
-if [[ tag =~ reg ]]
-then 
-    tag="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.$((${BASH_REMATCH[3]}+1))"
-    echo "new tag: $tag"
-    git tag "$tag"
-fi
+
+for fn in $(git tag); do
+    if [[$fn =~ reg]]
+        then
+            tag="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.$((${BASH_REMATCH[3]}+1))"
+            echo "new git tag: $tag"
+            git tag "$tag"
+            $TRAVIS_TAG=$tag
+    fi
+done
+
 git config --local user.name "Travis CI - Deploy"
 git config --local user.email "travis@travisci.org"
