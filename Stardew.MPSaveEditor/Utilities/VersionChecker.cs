ed967f0e596816fd754ab47e7f4f9ce2777f43e9
Eric Schwarz <egs3690@gmail.com>
2018-06-24 11:00:08 -0500

    Replaced verison checking serialization w/ regex to fix issues with stand-alone application. Generalized csproj os runtimes. Added win-x86 and linux-x64 to release deployment.

--- Stardew.MPSaveEditor/Utilities/VersionChecker.cs
+++ Stardew.MPSaveEditor/Utilities/VersionChecker.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net.Http;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -11,12 +12,13 @@ namespace StardewValley.MPSaveEditor.Utilities {
     public class VersionChecker {
         
         private static HttpClient gitClient = new HttpClient();
-
+        
         public String run(String currentVersion) {
             String result;
             currentVersion = currentVersion.Replace("v", "");
             var response = this.getReleases();
             var latestVersion = this.parseResponse(response);
+            // Need to have a separate path if the version is not valid or a try catch + raise exception from parseResponse.
             if (latestVersion == "FAILED") {
                 return "There was a problem checking the latest release. Please goto https://github.com/tfitz237/stardew-mp-save-editor/releases to see if there is a newer version";
             }
@@ -26,7 +28,7 @@ namespace StardewValley.MPSaveEditor.Utilities {
         
         private Stream getReleases() {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/repos/tfitz237/stardew-mp-save-editor/releases");
-            requestMessage.Headers.Add("User-Agent", "sd-mp-editor");
+            requestMessage.Headers.Add("User-Agent", "sd-mp-save-editor");
             var response = gitClient.SendAsync(requestMessage);
             Stream responseBody = response.Result.Content.ReadAsStreamAsync().Result;
             if (!response.IsCompletedSuccessfully) {
@@ -36,12 +38,19 @@ namespace StardewValley.MPSaveEditor.Utilities {
         }
 
         private String parseResponse(Stream responseBody) {
-            String versionStatus;
-            var serializer = new DataContractJsonSerializer(typeof(GitRelease[]));
+            String versionStatus = "0.0.0.0";
             try {
-                GitRelease[] gitReleases = (GitRelease[]) serializer.ReadObject(responseBody);
-                GitRelease release = (GitRelease) gitReleases.GetValue(0);
-                versionStatus = release.tagName.Replace("v", "");
+                Regex regex = new Regex("\"tag_name\":\"([^,]*)\"");
+                StreamReader responseReader = new StreamReader(responseBody);
+                String responseText = responseReader.ReadToEnd();
+                MatchCollection matches = regex.Matches(responseText);
+                foreach (Match match in matches) {
+                    String gitRelease = match.Groups[1].Value.Replace("v", "");
+                    Version releaseVersion = new Version(gitRelease);
+                    if (releaseVersion.CompareTo(new Version(versionStatus)) > 0) {
+                        versionStatus = gitRelease;
+                    }
+                }
             }
             catch (SerializationException) {
                 versionStatus = "FAILED";
@@ -55,8 +64,8 @@ namespace StardewValley.MPSaveEditor.Utilities {
         private String compareVersions(String currentVersionName, String latestVersionName) {
             String versionStatus;
 
-            var latestVersion = new Version(latestVersionName);
             var currentVersion = new Version(currentVersionName);
+            var latestVersion = new Version(latestVersionName);
             if (latestVersion.CompareTo(currentVersion) > 0) {
                 versionStatus = String.Format("Version {0} of the Stardew Valley Multiplayer Save Editor is now available. You can download it from https://github.com/tfitz237/stardew-mp-save-editor/releases.", latestVersionName);
             }
@@ -64,18 +73,6 @@ namespace StardewValley.MPSaveEditor.Utilities {
             versionStatus = "You are running the latest version of the Stardew Valley Multiplayer Save Editor.";
             }
             return versionStatus;
-        }
-
-        // static int Main(string[] args) {
-        //     VersionChecker versionChecker = new VersionChecker();
-        //     Console.WriteLine(versionChecker.checkVersion());
-        //     return 0;
-        // }
-
-        [DataContract]
-        private class GitRelease {
-            [DataMember(Name = "tag_name")]
-            public String tagName {get; set;}
         }
     }
 }
